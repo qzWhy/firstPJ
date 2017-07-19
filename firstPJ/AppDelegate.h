49fcc7e81057aa9dcb264397fc4db78798ab850e
qzWhy <fanqingzhong1@163.com>
2017-07-19 15:07:03 +0800

    init version

---
+++ firstPJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstPJ
+//
+//  Created by 000 on 17/7/19.
+//  Copyright © 2017年 faner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstPJ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  firstPJ
+//
+//  Created by 000 on 17/7/19.
+//  Copyright © 2017年 faner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
